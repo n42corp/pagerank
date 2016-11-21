a1cf5cc139ce7baba7b70ecc1c36c1a42b904a8c
Ed Jeon <ed@n42corp.com>
2016-11-21 10:47:24 +0900

    print count

--- python/user_rank_test.py
+++ python/user_rank_test.py
@@ -44,12 +44,29 @@ links, ids, idx_map = read_file(sys.argv[1], negative_file)
 
 pr = pageRank(links, alpha=0.85, convergence=0.00001, checkSteps=1)
 idx_sorted = np.argsort(pr)[::-1]
+center_index = np.where(idx_sorted == 0)[0][0]
+non_rank_value = pr[idx_sorted[center_index]]
+total_count = len(pr)
+positive_count = 0
+negative_count = 0
+for i in xrange(center_index, 0, -1):
+  if pr[idx_sorted[i]] > non_rank_value:
+    positive_count = i + 1
+    break
+for i in xrange(center_index, total_count):
+  if pr[idx_sorted[i]] < non_rank_value:
+    negative_count = total_count - i
+    break
+
+print "Total count: %d" % total_count
+print "Positive count: %d" % positive_count
+print "Neutral count: %d" % (total_count - positive_count - negative_count)
+print "Negative count: %d" % negative_count
 
 print "\ntop"
 for i in idx_sorted[:10]:
   print ids[i], "=", pr[i]
 
-center_index = np.where(idx_sorted == 0)[0][0]
 print "\ncenter ", center_index
 for i in idx_sorted[center_index-5:center_index+5]:
   print ids[i], "=", pr[i]
